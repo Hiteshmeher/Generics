e3dd2c5a81d018e85b0c59abf8c69f0be957db8f
Hiteshmeher <meher.hitesh4@gmail.com>
2022-08-28 12:05:05 +0530

    [Hitesh]: Extended the code to find the max more than 3

--- src/com/bridgelabz/generics/GenericsMain.java
+++ src/com/bridgelabz/generics/GenericsMain.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.generics;
+
 /**
  * 
  * @author HITESH
@@ -11,8 +12,8 @@ public class GenericsMain {
 	 */
 	public static void main(String[] args) {
 		// initializing the values and finding the maximum
-		System.out.println("Maximum of Integers is "+MaximumGenerics.toPrintMax(10,20,18));
-		System.out.println("Maximum of Float is " +MaximumGenerics.toPrintMax(10.7f,20.4f,17.7f));
-		System.out.println("Maximum of three Strings is " +MaximumGenerics.toPrintMax("Mango","Banana","Cherry"));
+		System.out.println("Maximum of Integers is " + MaximumGenerics.toPrintMax(10, 20, 18, 22));
+		System.out.println("Maximum of Float is " + MaximumGenerics.toPrintMax(10.7f, 20.4f, 17.7f, 9.99f));
+		System.out.println("Maximum of three Strings is " + MaximumGenerics.toPrintMax("StrawBerry", "Mango", "Banana", "Cherry"));
 	}
 }
